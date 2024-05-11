dacbac74d0f7e1206550ce1af76c965f31f739eb
ravshanismoilov <r.ismoilov1001@mail.com>
2024-05-11 22:33:02 +0500

    birorta gap

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import landing_page,GetTest
+
+
+urlpatterns =[
+    path('',landing_page,name='landing_page'),
+    path('test/',GetTest.as_view(),name='test')
+]
